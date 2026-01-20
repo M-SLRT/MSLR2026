e1b6da22bf37bbce374b592a3e9aa8a3b4114158
Ahmed Hasanaath <61200955+gufranSabri@users.noreply.github.com>
2026-01-21 01:51:03 +0300

    draft

---
+++ src/components/sections/Submission.jsx
@@ -0,0 +1,17 @@
+// src/components/sections/Schedule.jsx
+import React from 'react';
+import Submission from './Guidelines';
+import Dates from './Dates';
+
+
+const CallForPapers = () => {
+    return (
+        <>
+            <Submission />
+            <Dates />
+            
+        </>
+    );
+}
+
+export default CallForPapers;
